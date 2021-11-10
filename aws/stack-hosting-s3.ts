247f03fd271d61c9f68833f3f27b02b2b3f95fce
Cameron Childress <cameronc@gmail.com>
2021-11-10 13:52:02 -0500

    test deploy

--- aws/stack-hosting-s3.ts
+++ aws/stack-hosting-s3.ts
@@ -1,4 +1,5 @@
 import { Bucket } from "aws-cdk-lib/lib/aws-s3";
+import { BucketDeployment, Source } from "aws-cdk-lib/lib/aws-s3-deployment";
 import { Construct } from "constructs";
 
 export class S3Bucket extends Construct {
@@ -8,5 +9,10 @@ export class S3Bucket extends Construct {
     super(scope, id);
 
     this.bucket = new Bucket(this, "Website", { publicReadAccess: false });
+
+    new BucketDeployment(this, "Deployment", {
+      destinationBucket: this.bucket,
+      sources: [Source.asset("www/.vuepress/dist")],
+    });
   }
 }
