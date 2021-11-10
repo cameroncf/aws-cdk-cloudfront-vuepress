0251d5c7b5c16d80d019a320c570209fd5a4932e
Cameron Childress <cameronc@gmail.com>
2021-11-10 11:59:54 -0500

    test deploy

--- aws/stack-hosting.spec.ts
+++ aws/stack-hosting.spec.ts
@@ -1,9 +1,11 @@
-import { SynthUtils } from '@aws-cdk/assert';
-import { App } from 'aws-cdk-lib';
-import { HostingStack } from './stack-hosting';
+import { SynthUtils } from "@aws-cdk/assert";
+import { App } from "aws-cdk-lib";
+import { HostingStack } from "./stack-hosting";
 
-test('happy path', () => {
+test("happy path", () => {
   const app = new App();
-  const result = new HostingStack(app, 'Test');
+  const result = new HostingStack(app, "Test", {
+    env: { account: "foo", region: "bar" },
+  });
   expect(SynthUtils.toCloudFormation(result)).toMatchSnapshot();
 });
