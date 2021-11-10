9743684a25b3f979528d44a69de037af83946ec8
Cameron Childress <cameronc@gmail.com>
2021-11-10 11:52:37 -0500

    test deploy

--- aws/build.ts
+++ aws/build.ts
@@ -1,8 +1,13 @@
 import { App } from "aws-cdk-lib";
 import { HostingStack } from "./stack-hosting";
 
+const env = {
+  account: process.env.CDK_DEPLOY_ACCOUNT || process.env.CDK_DEFAULT_ACCOUNT,
+  region: process.env.CDK_DEPLOY_REGION || process.env.CDK_DEFAULT_REGION,
+};
+
 const app = new App();
 
-new HostingStack(app, "HostingStack");
+new HostingStack(app, "HostingStack", { env });
 
 app.synth();
--- aws/stack-hosting.ts
+++ aws/stack-hosting.ts
@@ -1,10 +1,10 @@
-import { Stack } from "aws-cdk-lib";
+import { Stack, StackProps } from "aws-cdk-lib";
 import { Construct } from "constructs";
 import { S3Bucket } from "./stack-hosting-s3";
 
 export class HostingStack extends Stack {
-  constructor(scope: Construct, id: string) {
-    super(scope, id);
+  constructor(scope: Construct, id: string, props: StackProps) {
+    super(scope, id, props);
 
     const { bucket } = new S3Bucket(this, "Bucket");
     // 1
